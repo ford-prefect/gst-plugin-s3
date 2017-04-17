46067eb40525a90a62f20b568ec2f2c2f1ab6127
Arun Raghavan <arun@arunraghavan.net>
2017-04-17 15:02:34 +0530

    Add scaffolding and skeleton source implementation
    It ... builds.

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+// Copyright (C) 2017 Author: Arun Raghavan <arun@arunraghavan.net>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the
+// Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#![crate_type="cdylib"]
+
+extern crate url;
+
+#[macro_use]
+extern crate gst_plugin;
+
+use gst_plugin::plugin::*;
+use gst_plugin::source::*;
+
+mod s3src;
+
+use s3src::S3Src;
+
+fn plugin_init(plugin: &Plugin) -> bool {
+    source_register(plugin,
+                    SourceInfo {
+                        name: "s3src".into(),
+                        long_name: "Amazon S3 Source".into(),
+                        description: "Reads an object from an S3 region and bucket".into(),
+                        classification: "Source/Network".into(),
+                        author: "Arun Raghavan <arun@arunraghavan.net>".into(),
+                        rank: 256,
+                        create_instance: S3Src::new_boxed,
+                        protocols: vec![],
+                        push_only: true,
+                    });
+
+    true
+}
+
+plugin_define!(b"s3src\0",
+               b"Amazon S3 Plugin\0",
+               plugin_init,
+               b"1.0\0",
+               b"LGPL-3.0\0",
+               b"s3\0",
+               b"s3\0",
+               b"https://github.com/ford-prefect/gst-plugin-s3\0",
+               b"2017-04-17\0");
---
+++ src/s3src.rs
@@ -0,0 +1,76 @@
+// Copyright (C) 2017 Author: Arun Raghavan <arun@arunraghavan.net>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the
+// Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+use url::Url;
+
+use gst_plugin::buffer::*;
+use gst_plugin::error::*;
+use gst_plugin::source::*;
+use gst_plugin::utils::*;
+
+pub struct S3Src {
+}
+
+impl S3Src {
+    pub fn new(element: Element) -> S3Src {
+        S3Src {
+        }
+    }
+
+    pub fn new_boxed(element: Element) -> Box<Source> {
+        Box::new(S3Src::new(element))
+    }
+}
+
+impl Source for S3Src {
+    fn uri_validator(&self) -> Box<UriValidator> {
+        Box::new(|url: &Url| -> Result<(), UriError> {
+            // FIXME
+            Ok(())
+        })
+    }
+
+    fn is_seekable(&self) -> bool {
+        // FIXME
+        false
+    }
+
+    fn get_size(&self) -> Option<u64> {
+        // FIXME
+        None
+    }
+
+    fn start(&mut self, uri: Url) -> Result<(), ErrorMessage> {
+        // FIXME
+        Ok(())
+    }
+
+    fn stop(&mut self) -> Result<(), ErrorMessage> {
+        // FIXME
+        Ok(())
+    }
+
+    fn fill(&mut self, offset: u64, length: u32, buffer: &mut Buffer) -> Result<(), FlowError> {
+        // FIXME
+        Ok(())
+    }
+
+    fn seek(&mut self, start: u64, stop: Option<u64>) -> Result<(), ErrorMessage> {
+        // FIXME
+        Ok(())
+    }
+}
