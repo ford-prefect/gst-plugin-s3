61a36994dc255874da0977cea969645505955212
Arun Raghavan <arun@arunraghavan.net>
2017-04-18 15:34:50 +0530

    Deal with slashes in object keys and add tests
    Removes the assumption that the object key won't consist of the '/'
    character (which is both perfectly acceptable and a common method of
    organisation). Since we previously had the version as the last part of
    the path, we now move that to a query parameter.
    
    Also adds some automated test cases so we can catch bugs early.

--- src/s3url.rs
+++ src/s3url.rs
@@ -48,27 +48,45 @@ pub fn parse_s3_url(url: &Url) -> Result<GstS3Url, UriError> {
                                        Some(format!("Invalid region '{}'", h))))
                  })?;
 
-    let path: Vec<&str> = url.path_segments().unwrap().collect();
-    if path.len() < 2 || path.len() > 3 {
-        return Err(UriError::new(UriErrorKind::BadUri, Some(format!("Invalid uri '{}'", url))));
-    }
+    let mut path =
+        url.path_segments()
+            .ok_or_else(|| {
+                            UriError::new(UriErrorKind::BadUri,
+                                          Some(format!("Invalid uri '{}'", url)))
+                        })?;
 
-    let bucket = path[0].to_string();
-    let object = path[1].to_string();
+    let bucket = path.next().unwrap().to_string();
 
-    if path[0].is_empty() || path[1].is_empty() {
+    let o = path.next()
+        .ok_or_else(|| {
+                        UriError::new(UriErrorKind::BadUri,
+                                      Some(format!("Invalid empty object/bucket '{}'", url)))
+                    })?;
+
+    let mut object = o.to_string();
+    if o.is_empty() {
         return Err(UriError::new(UriErrorKind::BadUri,
                                  Some(format!("Invalid empty object/bucket '{}'", url))));
     }
 
-    // Gets the string inside the Some (which can be), or an empty string on None
-    let ref v = path.get(2).map(|s| s.to_string()).unwrap_or_default();
+    object = path.fold(object, |o, p| format!("{}/{}", o, p));
+
+    let mut q = url.query_pairs();
+    let v = q.next();
     let version;
 
-    if v.is_empty() {
-        version = None;
-    } else {
-        version = Some(v.clone());
+    match v {
+        Some((ref k, ref v)) if k == "version" => version = Some((*v).to_string()),
+        None => version = None,
+        Some(_) => {
+            return Err(UriError::new(UriErrorKind::BadUri,
+                                     Some(format!("Bad query, only 'version' is supported"))));
+        }
+    }
+
+    if q.next() != None {
+        return Err(UriError::new(UriErrorKind::BadUri,
+                                 Some(format!("Extra query terms, only 'version' is supported"))));
     }
 
     Ok(GstS3Url {
@@ -77,4 +95,69 @@ pub fn parse_s3_url(url: &Url) -> Result<GstS3Url, UriError> {
            object: object,
            version: version,
        })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cannot_be_base() {
+        let url = Url::parse("data:something").unwrap();
+        assert!(parse_s3_url(&url).is_err());
+    }
+
+    #[test]
+    fn invalid_scheme() {
+        let url = Url::parse("file:///dev/zero").unwrap();
+        assert!(parse_s3_url(&url).is_err());
+    }
+
+    #[test]
+    fn bad_region() {
+        let url = Url::parse("s3://atlantis-1/i-hope-we/dont-find-this").unwrap();
+        assert!(parse_s3_url(&url).is_err());
+    }
+
+    #[test]
+    fn no_bucket() {
+        let url1 = Url::parse("s3://ap-south-1").unwrap();
+        assert!(parse_s3_url(&url1).is_err());
+        let url2 = Url::parse("s3://ap-south-1/").unwrap();
+        assert!(parse_s3_url(&url2).is_err());
+    }
+
+    #[test]
+    fn no_object() {
+        let url1 = Url::parse("s3://ap-south-1/my-bucket").unwrap();
+        assert!(parse_s3_url(&url1).is_err());
+        let url2 = Url::parse("s3://ap-south-1/my-bucket/").unwrap();
+        assert!(parse_s3_url(&url2).is_err());
+    }
+
+    #[test]
+    fn valid_simple() {
+        let url = Url::parse("s3://ap-south-1/my-bucket/my-object").unwrap();
+        assert!(parse_s3_url(&url).is_ok());
+    }
+
+    #[test]
+    fn extraneous_query() {
+        let url = Url::parse("s3://ap-south-1/my-bucket/my-object?foo=bar").unwrap();
+        assert!(parse_s3_url(&url).is_err());
+    }
+
+    #[test]
+    fn valid_version() {
+        let url = Url::parse("s3://ap-south-1/my-bucket/my-object?version=one").unwrap();
+        assert!(parse_s3_url(&url).is_ok());
+    }
+
+    #[test]
+    fn trailing_slash() {
+        // Slashes are valid at the end of the object key
+        let url = Url::parse("s3://ap-south-1/my-bucket/my-object/").unwrap();
+        assert!(parse_s3_url(&url).is_ok());
+    }
+
 }
